b556e99a7139d4b05ae30b3b8aa14d4a65a95c95
prochell <prochell@student.21-school.ru>
2022-05-12 15:46:57 +0300

    add simple projects

---
+++ TestProject01/app/src/main/java/com/example/testproject01/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.testproject01
+
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.example.testproject01.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingMain: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMain.root)
+
+        bindingMain.getRandomImage.setOnClickListener {
+            onGetRandomImagePressed()
+        }
+    }
+
+    private fun onGetRandomImagePressed() {
+
+        bindingMain.progressBar.visibility = View.VISIBLE
+        Glide.with(this)
+            .load("https://source.unsplash.com/random/800x600")
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .listener(object:RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    bindingMain.progressBar.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    bindingMain.progressBar.visibility = View.GONE
+                    return false
+                }
+            })
+//            .placeholder(R.drawable.ic_launcher_background)
+            .into(bindingMain.simpleImage)
+    }
+}
---
+++ TestSaveState/app/src/main/java/com/example/testsavestate/SaveStateWithSaveInstanceState.kt
@@ -0,0 +1,79 @@
+package com.example.testsavestate
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.testsavestate.databinding.ActivityMainBinding
+import kotlin.properties.Delegates.notNull
+import kotlin.random.Random
+
+class SaveStateWithSaveInstanceState: AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private var counterValue by notNull<Int>()
+    private var counterTextColor by notNull<Int>()
+    private var counterIsVisible by notNull<Boolean>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.incrementButton.setOnClickListener{ increment() }
+        binding.randomColorButton.setOnClickListener { setRandomColor() }
+        binding.switchVisibilityButton.setOnClickListener { switchVisibility() }
+
+        if (savedInstanceState == null) {
+            counterValue = 0
+            counterTextColor = ContextCompat.getColor(this, R.color.purple_700)
+            counterIsVisible = true
+        } else {
+            counterValue = savedInstanceState.getInt(KEY_COUNTER)
+            counterTextColor = savedInstanceState.getInt(KEY_COLOR)
+            counterIsVisible = savedInstanceState.getBoolean(KEY_IS_VISIBLE)
+        }
+        renderState()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_COUNTER, counterValue)
+        outState.putInt(KEY_COLOR, counterTextColor)
+        outState.putBoolean(KEY_IS_VISIBLE, counterIsVisible)
+    }
+
+    private fun switchVisibility() {
+        counterIsVisible = !counterIsVisible
+        renderState()
+    }
+
+    private fun setRandomColor() {
+        counterTextColor = Color.rgb(
+            Random.nextInt(256),
+            Random.nextInt(256),
+            Random.nextInt(256),
+        )
+        renderState()
+    }
+
+    private fun increment() {
+        counterValue++
+        renderState()
+    }
+
+    private fun renderState() = with(binding) {
+        counterText.text = counterValue.toString()
+        counterText.setTextColor(counterTextColor)
+        counterText.visibility = if (counterIsVisible) View.VISIBLE else View.INVISIBLE
+    }
+
+    companion object {
+        private const val KEY_COUNTER = "COUNTER"
+        private const val KEY_COLOR = "COLOR"
+        private const val KEY_IS_VISIBLE = "IS_VISIBLE"
+    }
+}
