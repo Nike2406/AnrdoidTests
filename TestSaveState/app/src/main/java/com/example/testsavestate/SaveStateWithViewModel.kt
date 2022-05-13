e30788f4e88561d5bb53ca6cbd9de015a0fba017
prochell <prochell@student.21-school.ru>
2022-05-13 18:00:01 +0300

    added viewModel method

---
+++ TestSaveState/app/src/main/java/com/example/testsavestate/SaveStateWithViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.testsavestate
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import com.example.testsavestate.databinding.ActivityMainBinding
+
+class SaveStateWithViewModel : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private val viewModel by viewModels<ViewModelLogic>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.incrementButton.setOnClickListener { viewModel.increment() }
+        binding.randomColorButton.setOnClickListener { viewModel.setRandomColor() }
+        binding.switchVisibilityButton.setOnClickListener { viewModel.switchVisibility() }
+
+        if (viewModel.state.value == null) {
+            viewModel.initState(
+                ViewModelLogic.State(
+                    counterValue = 0,
+                    counterTextColor = ContextCompat.getColor(this, R.color.purple_700),
+                    counterIsVisible = true,
+                )
+            )
+        }
+        viewModel.state.observe(this, Observer {
+            renderState(it)
+        })
+    }
+
+    private fun renderState(state: ViewModelLogic.State) = with(binding) {
+        counterText.text = state.counterValue.toString()
+        counterText.setTextColor(state.counterTextColor)
+        counterText.visibility = if (state.counterIsVisible) View.VISIBLE else View.INVISIBLE
+    }
+}
