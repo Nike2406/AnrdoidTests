c62c0e937b3ddc54c40cb5354c25ffa2d26e8d24
prochell <prochell@student.21-school.ru>
2022-05-13 18:15:15 +0300

    added combined save method with viewModel and parcelize

---
+++ TestSaveState/app/src/main/java/com/example/testsavestate/SaveStateWithViewModelParce.kt
@@ -0,0 +1,52 @@
+package com.example.testsavestate
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import com.example.testsavestate.databinding.ActivityMainBinding
+
+class SaveStateWithViewModelParce : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private val viewModel by viewModels<ViewModelLogicParce>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.incrementButton.setOnClickListener { viewModel.increment() }
+        binding.randomColorButton.setOnClickListener { viewModel.setRandomColor() }
+        binding.switchVisibilityButton.setOnClickListener { viewModel.switchVisibility() }
+
+        viewModel.initState(
+                savedInstanceState?.getParcelable(KEY_STATE) ?: ViewModelLogicParce.State(
+                    counterValue = 0,
+                    counterTextColor = ContextCompat.getColor(this, R.color.purple_700),
+                    counterIsVisible = true,
+                )
+            )
+        viewModel.state.observe(this, Observer {
+            renderState(it)
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(KEY_STATE, viewModel.state.value)
+    }
+
+    private fun renderState(state: ViewModelLogicParce.State) = with(binding) {
+        counterText.text = state.counterValue.toString()
+        counterText.setTextColor(state.counterTextColor)
+        counterText.visibility = if (state.counterIsVisible) View.VISIBLE else View.INVISIBLE
+    }
+
+    companion object {
+        @JvmStatic val KEY_STATE = "STATE"
+    }
+}
---
+++ TestSaveState/app/src/main/java/com/example/testsavestate/ViewModelLogicParce.kt
@@ -0,0 +1,54 @@
+package com.example.testsavestate
+
+import android.graphics.Color
+import android.os.Parcelable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.parcelize.Parcelize
+import kotlin.random.Random
+
+class ViewModelLogicParce : ViewModel() {
+
+    /*
+    * Позвооляет менять сосотояние только во viewModel,
+    * не изменяя его в activity */
+    val state: LiveData<State> get() = stateLiveData
+    private val stateLiveData = MutableLiveData<State>()
+
+    fun initState(state: State) {
+        stateLiveData.value = state
+    }
+
+    fun increment() {
+        val oldState = stateLiveData.value
+        stateLiveData.value = oldState?.copy(
+            counterValue = oldState.counterValue + 1
+        )
+    }
+
+    fun switchVisibility() {
+        val oldState = stateLiveData.value
+        stateLiveData.value = oldState?.copy(
+            counterIsVisible = !oldState.counterIsVisible
+        )
+    }
+
+    fun setRandomColor() {
+        val oldState = stateLiveData.value
+        stateLiveData.value = oldState?.copy(
+            counterTextColor = Color.rgb(
+                Random.nextInt(256),
+                Random.nextInt(256),
+                Random.nextInt(256),
+            )
+        )
+    }
+
+    @Parcelize
+    data class State(
+        var counterValue: Int,
+        var counterTextColor: Int,
+        var counterIsVisible: Boolean,
+    ) : Parcelable
+}
