b93474754614e5a265edd6a34aa64dae300b1864
prochell <prochell@student.21-school.ru>
2022-05-19 19:21:33 +0300

    Added project

---
+++ SimpleAdapter/app/src/main/java/com/example/simpleadapter/SimpleAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.simpleadapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SimpleAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
