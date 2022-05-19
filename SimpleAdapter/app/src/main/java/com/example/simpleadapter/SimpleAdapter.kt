8ef5a99986f4438780417fc8e920b09f7a91bca6
prochell <prochell@student.21-school.ru>
2022-05-19 20:19:15 +0300

    Added main and item layout and raw main.kt

--- SimpleAdapter/app/src/main/java/com/example/simpleadapter/SimpleAdapter.kt
+++ SimpleAdapter/app/src/main/java/com/example/simpleadapter/SimpleAdapter.kt
@@ -1,11 +1,41 @@
 package com.example.simpleadapter
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simpleadapter.databinding.ActivityMainBinding
 
 class SimpleAdapter : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setupListViewSingle()
+        setContentView(binding.root)
+    }
+
+    private fun setupListViewSingle() {
+
+        val adapterElements = listOf(
+            mapOf(
+                KEY_TITLE to "First title",
+                KEY_DESCRIPTION to "First description"
+            ),
+            mapOf(
+                KEY_TITLE to "Second title",
+                KEY_DESCRIPTION to "Second description"
+            ),
+            mapOf(
+                KEY_TITLE to "Third title",
+                KEY_DESCRIPTION to "Third description"
+            ),
+        )
+    }
+
+    companion object {
+        @JvmStatic val KEY_TITLE = "title"
+        @JvmStatic val KEY_DESCRIPTION = "description"
     }
 }
